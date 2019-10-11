8d7f3161b0fb3cf37d07a5849e4249aa2a0b1361
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2019-10-11 16:39:08 +0100

    Added comments
    Also changed some bash Linux command to windows command

--- finetune_squad_2_0_py.py
+++ finetune_squad_2_0_py.py
@@ -3,57 +3,68 @@ import numpy as np
 import pandas as pd
 import zipfile
 from matplotlib import pyplot as plt
-# %matplotlib inline
+%matplotlib inline
 import sys
 import datetime
+import os
 
 #mounting google drive to colab
 from google.colab import drive
 drive.mount('/content/drive')
-
-import os
 print(os.listdir("/content/drive/My Drive/SQuAD JSON-v2.0"))
 
+!wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip
 
-!wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
+REPO = 'model_repo'
+with zipfile.ZipFile("uncased_L-24_H-1024_A-16.zip","r") as zip_ref:
+    zip_ref.extractall(REPO)
+ 
+#Listing the files in the model
+!dir 'model_repo/uncased_L-24_H-1024_A-16'
 
-repo = 'model_repo'
-with zipfile.ZipFile("uncased_L-12_H-768_A-12.zip","r") as zip_ref:
-    zip_ref.extractall(repo)
+#Listing files in the model_repo
+!dir 'model_repo'
 
-!ls 'model_repo/uncased_L-12_H-768_A-12'
-
-!ls 'model_repo'
-
+#Downloading some python files from Google's BERT needed for training
 !wget https://raw.githubusercontent.com/google-research/bert/master/modeling.py 
 !wget https://raw.githubusercontent.com/google-research/bert/master/optimization.py 
 !wget https://raw.githubusercontent.com/google-research/bert/master/run_squad.py 
 !wget https://raw.githubusercontent.com/google-research/bert/master/tokenization.py
 
-BERT_MODEL = 'uncased_L-12_H-768_A-12'
-BERT_PRETRAINED_DIR = f'{repo}/uncased_L-12_H-768_A-12'
-OUTPUT_DIR = f'{repo}/outputs'
+#Creating an output directory for our output
+#Creating a bert pretrained directory
+#Available pretrained model checkpoints:
+#uncased_L-12_H-768_A-12: uncased BERT base model
+#uncased_L-24_H-1024_A-16: uncased BERT large model
+#cased_L-12_H-768_A-12: cased BERT large model
+BERT_MODEL = 'uncased_L-24_H-1024_A-16'
+BERT_PRETRAINED_DIR = f'{REPO}/uncased_L-24_H-1024_A-16'
+OUTPUT_DIR = f'{REPO}/outputs'
 print(f'***** Model output directory: {OUTPUT_DIR} *****')
 print(f'***** BERT pretrained directory: {BERT_PRETRAINED_DIR} *****')
 
+#The wget command helps us to download the SQuAD 2.0 dataset from the internet
 !wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
 !wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
 !wget https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
+!dir
 
-!ls
-
+#Renaming our index.html file
 !mv index.html evaluate-v2.0.py
 
-!ls model_repo/uncased_L-12_H-768_A-12
+!dir model_repo/uncased_L-24_H-1024_A-16
 
+#Creating a new directory
 !mkdir output
 
+#print full system path of output directory
 !pwd output/
 
+#Code to run the run_squad.py file with training set and test set of SQuAD 2.0
 !python3 run_squad.py \
---vocab_file=model_repo/uncased_L-12_H-768_A-12/vocab.txt \
---bert_config_file=model_repo/uncased_L-12_H-768_A-12/bert_config.json \
---init_checkpoint=model_repo/uncased_L-12_H-768_A-12/bert_model.ckpt \
+--vocab_file=model_repo/uncased_L-24_H-1024_A-16/vocab.txt \
+--bert_config_file=model_repo/uncased_L-24_H-1024_A-16/bert_config.json \
+--init_checkpoint=model_repo/uncased_L-24_H-1024_A-16/bert_model.ckpt \
 --do_train=False \
 --train_file=train-v2.0.json \
 --do_predict=True \
@@ -64,4 +75,6 @@ print(f'***** BERT pretrained directory: {BERT_PRETRAINED_DIR} *****')
 --max_seq_length=384 \
 --doc_stride=128 \
 --version_2_with_negative=True \
---output_dir=/content/output
+--output_dir=/tmp/squad_large/
+
+#python evaluate-v2.0.py dev-v2.0.json ./squad/predictions.json --na-prob-file ./squad/null_odds.json
